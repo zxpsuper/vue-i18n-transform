b2afcd1fdb83cbef28b1f5586f1be65e018a0f1f
zhengxiaopeng <zxpscau@163.com>
2024-04-16 09:37:49 +0800

    feat: 更新0.0.8

--- src/lib/recoverJson.ts
+++ src/lib/recoverJson.ts
@@ -10,6 +10,28 @@ const fs = require('fs-extra')
 const path = require('path')
 const configFile = 'vue-i18n-transform.config'
 
+/**初始化文件 */
+function initFile() {
+  const config = VueI18nInstance.getConfig()
+  // @ts-ignore
+  const i18nDir = path.join(config.projectDirname, config.outdir)
+  const i18nFile = path.join(i18nDir, `${config.filename}.json`)
+
+  if (fs.existsSync(i18nFile)) {
+    const data = fs.readFileSync(i18nFile)
+    const i18nObj = !!data.toString() ? JSON.parse(data.toString()) : {}
+    try {
+      VueI18nInstance.deleteMessages()
+      VueI18nInstance.setMessage(i18nObj)
+    } catch (e: any) {
+      return msg.error(e.message || 'setMessage error')
+    }
+  } else {
+    VueI18nInstance.setMessage({})
+  }
+  VueI18nInstance.initIndex()
+}
+
 export default function ({
   editor,
   context
@@ -26,6 +48,7 @@ export default function ({
     const filePath = currentEditor.document.fileName
     const options = getCustomSetting(filePath, configFile)
     VueI18nInstance.mergeConfig(options)
+    initFile()
     path.extname(filePath).toLowerCase() === '.vue'
       ? recoverVueFile(filePath)
       : recoverJsFile(filePath)
--- test.ts
+++
@@ -1,3 +0,0 @@
-const a = undefined
-
-const t = a?? 1 : 0
